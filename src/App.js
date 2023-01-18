ae4419ac295aae68318ae05333c85bb16bfade59
nishith-7 <nishith.upadhyay@techholding.co>
2023-01-18 10:25:32 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,18 @@
 //import logo from './logo.svg';
 import './App.css';
 import React from "react";
-import User from "./userData.json";
+import User from "./User";
+
 
 function App() {
-  const temp = User.filter(user => user.index === 3);
-  // console.log(temp);
+  // console.log(temp)
+  
   return (
-    <div className="container">
     <div className="App">
-      
-      
-        <div className="row">
-       <strong> Name: </strong>
-       {temp[0].name}
-        </div> 
-        <br></br>
-        <div className="row">
-          <strong>Company: </strong>
-          {temp[0].company}
-        </div>
-        <br>
-        </br>
-        <div ClassName="row">
-          <strong>Email: </strong>
-          {temp[0].email}
-        </div>
-        <br></br>
-        <div ClassName="row">
-          <strong>More Info: </strong>
-          {temp[0].about}
-        </div>
+  
+         <User index="3" />
+    
     </div>
-        </div>
-   
   );
 }
 
